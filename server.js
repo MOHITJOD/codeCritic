7ca82176dc31a3df6106349aae3aa51d82b84415
MOHITJOD <mohitfpp@gmail.com>
2025-07-18 20:17:19 +0530

    not impo

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 require('dotenv').config()
 const app = require('./BackEnd/src/app')
 
-import path from 'path';
-import express from 'express';
-
-const app = express();
-
-// Serve frontend static files
-app.use(express.static(path.join(process.cwd(), 'public')));
-
-app.get('*', (_, res) => {
-  res.sendFile(path.join(process.cwd(), 'public/index.html'));
-});
-
-// your existing API routes…
 
 
 app.listen(3000, () => {
