63112807d3d0dc18123d58998319016eb9f95e82
MOHITJOD <mohitfpp@gmail.com>
2025-07-18 20:53:46 +0530

    deploy

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
+require('dotenv').config()
+const path = require('path')
 
 const app = express()
 
@@ -8,11 +10,17 @@ app.use(cors())
 
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
 app.use('/ai', aiRoutes)
+
+// Serve index.html for all other routes (for SPA support)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 module.exports = app
