491eb4e58fb4945930924897749542cb29fbe4f0
MOHITJOD <mohitfpp@gmail.com>
2025-07-18 23:12:12 +0530

    done maybe this time

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
+const path = require('path');
 
 const app = express()
 
@@ -8,11 +9,17 @@ app.use(cors())
 
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.use('/ai', aiRoutes)
+app.use('/ai', aiRoutes);
+
+// Serve React app for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app
